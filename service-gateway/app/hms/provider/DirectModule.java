087f64b8de6a1ef7ac88db819bf4c07493fbfe2b
AnhNghia Nguyen <nghianghesi@gmail.com>
2019-05-25 10:10:57 +0700

    provider direct module config

--- service-gateway/app/hms/provider/DirectModule.java
+++ service-gateway/app/hms/provider/DirectModule.java
@@ -5,6 +5,9 @@ import com.google.inject.AbstractModule;
 public class DirectModule extends AbstractModule {
 	@Override
 	protected void configure() {		
+        bind(hms.hub.repositories.IHubNodeRepository.class).to(hms.hub.repositories.HubNodeRepository.class);        
+        bind(hms.hub.IHubService.class).to(hms.hub.HubService.class);
+        
         bind(hms.provider.repositories.IProviderRepository.class).to(hms.provider.repositories.ProviderRepository.class);
         bind(hms.provider.IProviderService.class).to(hms.provider.ProviderService.class);        
 	}
