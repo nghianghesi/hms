fb3bdae1bd811c557e46b671ba96918d590322cb
AnhNghia Nguyen <nghianghesi@gmail.com>
2019-06-06 08:05:31 +0700

    remove log

--- kafka-service-common/src/main/java/hms/kafka/streamming/KafkaStreamNodeBase.java
+++ kafka-service-common/src/main/java/hms/kafka/streamming/KafkaStreamNodeBase.java
@@ -121,7 +121,7 @@ public abstract class KafkaStreamNodeBase<TCon, TRep>{
 	
 	private void processSingleRecord(ConsumerRecord<UUID, byte[]> record) {
 		try {
-			this.getLogger().info("Consuming {} {}", this.getConsumeTopic(), record.key());					
+			//this.getLogger().info("Consuming {} {}", this.getConsumeTopic(), record.key());					
 			HMSMessage<TCon> request = KafkaMessageUtils.getHMSMessage(this.getTConsumeManifest(), record);											
 			TRep res = this.processRequest(request);
 			if(this.getForwardTopic()!=null) {
