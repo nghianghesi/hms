19c2c76ed69b51f5ed937f0002f8003a07a5ffc8
AnhNghia Nguyen <nghianghesi@gmail.com>
2019-06-09 22:50:55 +0700

    kafka provider processing

--- processing-host/app/hms/provider/KafkaConsumerModule.java
+++ processing-host/app/hms/provider/KafkaConsumerModule.java
@@ -27,7 +27,8 @@ public class KafkaConsumerModule extends AbstractModule {
 	}	
 	
 	@Override
-	protected void configure() {
+	protected void configure() {        
+		bind(hms.hub.repositories.IHubNodeRepository.class).to(hms.hub.repositories.HubNodeRepository.class);
         bind(hms.provider.repositories.IProviderRepository.class).to(hms.provider.repositories.ProviderRepository.class);
         bind(hms.provider.IProviderService.class).to(hms.provider.ProviderService.class);
         bind(hms.hub.IHubService.class).to(hms.hub.HubService.class);
